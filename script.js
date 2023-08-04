a8ff4f7fe0cd40f4c030f20ef44d2f0dfde49124
Lucas Eduardo <lucas.2013costa@hotmail.com>
2023-08-04 14:27:37 -0400

    adições no html

---
+++ script.js
@@ -0,0 +1,22 @@
+var nome =  'Andre';
+var idade = 28;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade, nome);
+
+var preco = 25;
+var totalComprado = 5;
+var totalComprado = totalComprado + preco;
+
+console.log(totalComprado);
+
+x = 5; // Atribuição de valor a uma variável não declarada
+console.log(x); // 5 é exibido no console se não seria undefined
+
+foo(); // Chamada da função foo antes de declará-la
+
+console.log(5 !== "5"); // true (os valores são diferentes e os tipos também)
+console.log(5 !== 10); // true (os valores são diferentes)
+
+// o operador !== compara os valores sem fazer a conversão de tipo.
+
